b4a59a75cc2f718e3178b6ef1372358720188ed6
Atnanasi <ys2000pro@gmail.com>
2017-10-01 02:36:53 +0900

    なんかうごいた

--- src/controller.py
+++ src/controller.py
@@ -14,9 +14,37 @@ def get_articles(config):
 
     articles = []
 
-    for row in list(db.execute("select * from articles")):
+    for rowt in list(db.execute("select * from articles")):
+        row = dict(rowt)
         row["date"] = datetime.fromtimestamp(row["unixtime"]).strftime(time_format)
-        row["url"] = url + row["name"]
+        row["url"] = url + "/article/" + str(row["id"])
         articles.append(row)
 
     return articles
+
+def get_article(id, config):
+    dbname = "database/" + config["system"]["dbname"]
+    url = config["blog"]["url"]
+    time_format = config["system"]["time_format"]
+
+    dbconn = sqlite3.connect(dbname)
+    db = dbconn.cursor()
+    db.row_factory = sqlite3.Row
+
+    article = {}
+
+    for row in list(db.execute("select * from articles where id=?", (id))):
+        article = dict (row)
+        article["date"] = datetime.fromtimestamp(article["unixtime"]).strftime(time_format)
+        article["url"] = url + "/article/" + str(article["id"])
+
+    return article
+
+def exist_article(id, config):
+    dbname = "database/" + config["system"]["dbname"]
+
+    dbconn = sqlite3.connect(dbname)
+    db = dbconn.cursor()
+    db.row_factory = sqlite3.Row
+
+    return True if db.execute("select * from articles where id=?", (id)) != "" else False
--- src/main.py
+++ src/main.py
@@ -13,7 +13,13 @@ app = Flask("feelest")
 @app.route('/')
 def index():
     articles = controller.get_articles(config)
-    return render_template('index.html',blog=config["blog"],articles=articles)
+    return render_template('index.tmpl', blog=config["blog"], articles=articles)
+
+@app.route('/article/<string:id>')
+def article(id):
+    if controller.exist_article(id, config):
+        article_data = dict(controller.get_article(id, config))
+        return render_template('article.tmpl', blog=config["blog"], article=article_data, is_article=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
