da8acb83e78e920cce36e7dd2e3c8dcfd53ea245
Atnanasi <ys2000pro@gmail.com>
2018-01-22 16:00:10 +0900

    rename controller

--- src/main.py
+++ src/main.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 
-import configparser
+from configparser import ConfigParser
 from flask import Flask, render_template, redirect, request, session
 from flask_httpauth import HTTPDigestAuth
 import sqlite3
 import tweepy
 
-import controller
+import articles
+import auth
 
-config = configparser.ConfigParser()
+config = ConfigParser()
 config.read("config/feelest.ini", encoding="utf-8")
 
 app = Flask("feelest")
 
 @app.route("/")
 def index():
-    articles = controller.get_articles(invisible=True, config=config)
-    return render_template("index.tmpl", blog=config["blog"], articles=articles)
+    article_list = articles.get_articles(invisible=True, config=config)
+    return render_template("index.tmpl", blog=config["blog"], articles=article_list)
 
 @app.route("/article/<string:id>")
 def article(id):
-    if controller.exist_article(id=id, invisible=True, config=config):
-        article_data = dict(controller.get_article(id=id, invisible=True, config=config))
+    if articles.exist_article(id=id, invisible=True, config=config):
+        article_data = dict(articles.get_article(id=id, invisible=True, config=config))
         return render_template("article.tmpl", blog=config["blog"], article=article_data, is_article=True)
 
 @app.route("/login", methods=["GET", "POST"])
