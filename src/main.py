bff95908026901d5d53e46714998b4f3439ec7d5
Atnanasi <ys2000pro@gmail.com>
2018-01-22 16:07:37 +0900

    compliance with pylint

--- src/main.py
+++ src/main.py
@@ -9,34 +9,50 @@ import tweepy
 import articles
 import auth
 
-config = ConfigParser()
-config.read("config/feelest.ini", encoding="utf-8")
+CONFIG = ConfigParser()
+CONFIG.read("config/feelest.ini", encoding="utf-8")
 
-app = Flask("feelest")
+APP = Flask("feelest")
 
-@app.route("/")
+dbconnector = sqlite3.connect("database/" + CONFIG["system"]["dbname"])
+dbcursor = dbconnector.cursor()
+dbconnector.row_factory = sqlite3.Row
+
+@APP.route("/")
 def index():
-    article_list = articles.get_articles(invisible=True, config=config)
-    return render_template("index.tmpl", blog=config["blog"], articles=article_list)
+    """
+    Return index page
+    """
+    article_list = articles.get_articles(invisible=True, config=CONFIG)
+    return render_template("index.tmpl", blog=CONFIG["blog"], articles=article_list)
 
-@app.route("/article/<string:id>")
+@APP.route("/article/<string:id>")
 def article(id):
-    if articles.exist_article(id=id, invisible=True, config=config):
-        article_data = dict(articles.get_article(id=id, invisible=True, config=config))
-        return render_template("article.tmpl", blog=config["blog"], article=article_data, is_article=True)
+    """
+    Return article page
+    """
+    if articles.exist_article(id=id, invisible=True, config=CONFIG):
+        article_data = dict(articles.get_article(id=id, invisible=True, config=CONFIG))
+        return render_template("article.tmpl", blog=CONFIG["blog"], article=article_data, is_article=True)
 
-@app.route("/login", methods=["GET", "POST"])
+@APP.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    Login with digest or oauth
+    """
     if request.method == "POST":
-        if config["system"]["authenticator"] == "digest":
+        if CONFIG["system"]["authenticator"] == "digest":
             session["username"] = request.form["username"]
             return redirect(request.referrer or "/")
     return render_template("login.tmpl")
 
-@app.route("/logout")
+@APP.route("/logout")
 def logout():
+    """
+    Logout from feelest
+    """
     session.pop("session_token", None)
     return redirect(request.referrer or "/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    APP.run(host="0.0.0.0")
